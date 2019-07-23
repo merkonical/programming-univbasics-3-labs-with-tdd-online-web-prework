8c6d29ea90f8027fa70f8d2ff3ffa1a669234a2b
Maverick Adolfo <merkonical@gmail.com>
2019-07-23 05:56:20 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,6 @@
-# Add your variables here
+first_number = 30 
+second_number = 1 
+sum = first_number + second_number
+difference = 30 - 1 
+product = first_number * second_number
+quotient = first_number / second_number
